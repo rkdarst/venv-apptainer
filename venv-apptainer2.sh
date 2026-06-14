1aac39ef114637187cd3e586746398be13829aed
Richard Darst <rkd@zgib.net>
2026-06-14 13:25:41 +0300

    venv-apptainer2: add squashfs support

--- venv-apptainer2.sh
+++ venv-apptainer2.sh
@@ -4,9 +4,10 @@ function vea() {
     #set -x
     local IMG
     IMG="$VENV_APPTAINER_IMAGE"
-    local PATH_IN PATH_OUT
+    local PATH_IN PATH_OUT BIND_VENV
     PATH_IN=/venv-apptainer/
-    PATH_OUT=venva/venv/
+    PATH_OUT=$PWD/venva/venv/
+    BIND_VENV=--bind=$PATH_OUT:$PATH_IN
 
     if test -e venva/ ; then
 	PATH="$PWD"/venva/venva-bin/:"$PATH"
@@ -16,25 +17,32 @@ function vea() {
     fi
 
     mkdir -p $PATH_OUT
-    mkdir -p venva/venva-bin/
+    mkdir -p venva/bin
     mkdir -p $HOME/.cache/pip-apptainer
     apptainer exec --no-home \
-	      --bind $PWD/$PATH_OUT:$PATH_IN \
+	      $BIND_VENV \
 	      --bind $HOME/.cache/pip-apptainer/:$HOME/.cache/pip \
 	      "$IMG" \
 	      bash -c 'python3 -m venv /venv-apptainer ; source /venv-apptainer/bin/activate ; pip install -r requirements.txt'
 
-    echo apptainer exec --no-home --bind $PWD/$PATH_OUT:$PATH_IN --env LANG= "$IMG" 'bash -c "source /venv-apptainer/bin/activate ; \${@:-bash}" - "$@"' > venva/exec
+    if type mksquashfs > /dev/null ; then
+	mksquashfs venva/venv/ venva/venv.squashfs
+	PATH_OUT=$PWD/venva/venv.squashfs
+	BIND_VENV=--bind=$PATH_OUT:$PATH_IN:image-src=/
+	rm -r venva/venv/
+    fi
+
+    echo apptainer exec --no-home $BIND_VENV --env LANG= "$IMG" 'bash -c "source /venv-apptainer/bin/activate ; \${@:-bash}" - "$@"' > venva/exec
     #echo apptainer exec --no-home --bind $PWD/venva:/venv --env LANG= "$IMG" 'bash -c "source /venv-apptainer/bin/activate ; python \$@" - "$@"' > venva/python
     #echo apptainer exec --no-home --bind $PWD/venva:/venv --env LANG= "$IMG" 'bash -c "source /venv-apptainer/bin/activate ; pip \$@" - "$@"' > venva/pip
     chmod a+x venva/exec
 
     for executable in $(./venva/exec ls $PATH_IN/bin/) ; do
-	echo "$PWD"/venva/exec "$executable" '"$@"' >> venva/venva-bin/"$executable"
-	chmod a+x venva/venva-bin/"$executable"
+	echo "$PWD"/venva/exec "$executable" '"$@"' >> venva/bin/"$executable"
+	chmod a+x venva/bin/"$executable"
     done
 
-    PATH="$PWD"/venva/venva-bin/:"$PATH"
+    PATH="$PWD"/venva/bin/:"$PATH"
     VIRTUAL_ENV=venva
     #set +x
 }
