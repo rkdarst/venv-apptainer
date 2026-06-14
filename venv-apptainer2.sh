29eb835f4d610cd01d70f19f712713bd5997edb3
Richard Darst <rkd@zgib.net>
2026-06-14 12:32:59 +0300

    venv-apptainer2: parameterize paths

--- venv-apptainer2.sh
+++ venv-apptainer2.sh
@@ -1,10 +1,12 @@
-echo $BASH_SOURCE
 VENV_APPTAINER_IMAGE=$(readlink -f $(dirname $BASH_SOURCE)/python-3.14.sif)
 
 function vea() {
     #set -x
     local IMG
     IMG="$VENV_APPTAINER_IMAGE"
+    local PATH_IN PATH_OUT
+    PATH_IN=/venv-apptainer/
+    PATH_OUT=venva/venv/
 
     if test -e venva/ ; then
 	PATH="$PWD"/venva/venva-bin/:"$PATH"
@@ -13,21 +15,21 @@ function vea() {
 	return
     fi
 
-    mkdir -p venva
-    mkdir -p venva/venva-bin
+    mkdir -p $PATH_OUT
+    mkdir -p venva/venva-bin/
     mkdir -p $HOME/.cache/pip-apptainer
     apptainer exec --no-home \
-	      --bind $PWD/venva/:/venv-apptainer \
+	      --bind $PWD/$PATH_OUT:$PATH_IN \
 	      --bind $HOME/.cache/pip-apptainer/:$HOME/.cache/pip \
 	      "$IMG" \
 	      bash -c 'python3 -m venv /venv-apptainer ; source /venv-apptainer/bin/activate ; pip install -r requirements.txt'
 
-    echo apptainer exec --no-home --bind $PWD/venva:/venv-apptainer --env LANG= "$IMG" 'bash -c "source /venv-apptainer/bin/activate ; \${@:-bash}" - "$@"' > venva/exec
+    echo apptainer exec --no-home --bind $PWD/$PATH_OUT:$PATH_IN --env LANG= "$IMG" 'bash -c "source /venv-apptainer/bin/activate ; \${@:-bash}" - "$@"' > venva/exec
     #echo apptainer exec --no-home --bind $PWD/venva:/venv --env LANG= "$IMG" 'bash -c "source /venv-apptainer/bin/activate ; python \$@" - "$@"' > venva/python
     #echo apptainer exec --no-home --bind $PWD/venva:/venv --env LANG= "$IMG" 'bash -c "source /venv-apptainer/bin/activate ; pip \$@" - "$@"' > venva/pip
     chmod a+x venva/exec
 
-    for executable in $(./venva/exec ls /venv-apptainer/bin/) ; do
+    for executable in $(./venva/exec ls $PATH_IN/bin/) ; do
 	echo "$PWD"/venva/exec "$executable" '"$@"' >> venva/venva-bin/"$executable"
 	chmod a+x venva/venva-bin/"$executable"
     done
