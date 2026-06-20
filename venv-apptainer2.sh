67ac98e31b3ebfdcc3a4ef418680720a7481b805
Richard Darst <rkd@zgib.net>
2026-06-20 15:00:54 +0300

    venv-apptainer2: untabify

--- venv-apptainer2.sh
+++ venv-apptainer2.sh
@@ -18,19 +18,19 @@ function vea() {
     local PATH_IN=/venv-apptainer/   # mountd path in the container
 
     while [[ $# -gt 0 ]]; do
-	case $1 in
-	    -h|--help)
-		echo "usage: vea REQ_FILE"
-		echo
-		echo -- "REQ_FILE:      Use this requirements file (default: detect"
+        case $1 in
+            -h|--help)
+                echo "usage: vea REQ_FILE"
+                echo
+                echo -- "REQ_FILE:      Use this requirements file (default: detect"
                 echo -- "               environment.yml, pylock.toml, requirements.txt"
                 echo -- "               in that order)"
-		echo -- "--force:       force a rebuild"
-		echo -- "--no-squash:   Don't compact to a squashfs filesystem.  Allows"
-		echo -- "               updating/editing later"
-		echo -- "--pip:         Install file with pip"
-		echo -- "--conda:       Install file with conda"
-		shift
+                echo -- "--force:       force a rebuild"
+                echo -- "--no-squash:   Don't compact to a squashfs filesystem.  Allows"
+                echo -- "               updating/editing later"
+                echo -- "--pip:         Install file with pip"
+                echo -- "--conda:       Install file with conda"
+                shift
                 return
                 ;;
             -f|--force)
@@ -75,22 +75,22 @@ function vea() {
     elif [ "$install_type" = conda ] ; then
         test -n "$REQ_FILE" || local REQ_FILE=environment.yml
     elif [ -n "$REQ_FILE" ] ; then
-	case "$REQ_FILE" in
-	    *.txt|*.toml)
-		local install_type=pip
-	    ;;
-	    *.yml)
-		local install_type=conda
-	    ;;
-	esac
+        case "$REQ_FILE" in
+            *.txt|*.toml)
+                local install_type=pip
+            ;;
+            *.yml)
+                local install_type=conda
+            ;;
+        esac
     elif [ -e environment.yml ]  ; then local install_type=conda ; local REQ_FILE=environment.yml
     elif [ -e pylock.toml ]      ; then local install_type=pip   ; local REQ_FILE=pylock.toml
-    elif [ -e requirements.txt ] ; then	local install_type=pip   ; local REQ_FILE=requirements.txt
+    elif [ -e requirements.txt ] ; then local install_type=pip   ; local REQ_FILE=requirements.txt
     fi
     # Warn if nothing was detected
     if [ -z "$REQ_FILE" ] ; then
-	echo "No requirements file auto-detected"
-	return 1
+        echo "No requirements file auto-detected"
+        return 1
     fi
     echo "Installing $REQ_FILE with mode $install_type"
 
@@ -98,26 +98,26 @@ function vea() {
     local install_type install_command
     local IMG BIND_CACHE
     if [ "$install_type" = pip ] ; then
-	if ! test -e "$VENV_APPTAINER_IMAGE" ; then
-	    apptainer pull "$VENV_APPTAINER_IMAGE" docker://python:3.13.14-trixie
-	fi
-	install_command="python3 -m venv /venv-apptainer ; source /venv-apptainer/bin/activate ; pip install -r ${REQ_FILE:-requirements.txt}"
-	IMG="$VENV_APPTAINER_IMAGE"
-	mkdir -p "$HOME"/.cache/pip-apptainer
-	BIND_CACHE=--bind="$HOME"/.cache/pip-apptainer/:"$HOME"/.cache/pip
-	SQUASHFS_FILE=venv.squashfs
+        if ! test -e "$VENV_APPTAINER_IMAGE" ; then
+            apptainer pull "$VENV_APPTAINER_IMAGE" docker://python:3.13.14-trixie
+        fi
+        install_command="python3 -m venv /venv-apptainer ; source /venv-apptainer/bin/activate ; pip install -r ${REQ_FILE:-requirements.txt}"
+        IMG="$VENV_APPTAINER_IMAGE"
+        mkdir -p "$HOME"/.cache/pip-apptainer
+        BIND_CACHE=--bind="$HOME"/.cache/pip-apptainer/:"$HOME"/.cache/pip
+        SQUASHFS_FILE=venv.squashfs
     elif [ "$install_type" = conda ] ; then
-	if ! test -e "$CONDA_APPTAINER_IMAGE" ; then
-	    apptainer pull "$CONDA_APPTAINER_IMAGE" docker://condaforge/miniforge3:26.3.2-3
-	fi
-	install_command="conda env create --yes -p /venv-apptainer -f ${REQ_FILE:-environment.yml}"
-	IMG="$CONDA_APPTAINER_IMAGE"
-	mkdir -p "$HOME"/.cache/conda-apptainer "$HOME"/.conda-apptainer/
-	BIND_CACHE="--bind=$HOME/.cache/conda-apptainer/:$HOME/.cache/conda/ --bind=$HOME/.conda-apptainer/:$HOME/.conda/"
-	SQUASHFS_FILE=conda-env.squashfs
+        if ! test -e "$CONDA_APPTAINER_IMAGE" ; then
+            apptainer pull "$CONDA_APPTAINER_IMAGE" docker://condaforge/miniforge3:26.3.2-3
+        fi
+        install_command="conda env create --yes -p /venv-apptainer -f ${REQ_FILE:-environment.yml}"
+        IMG="$CONDA_APPTAINER_IMAGE"
+        mkdir -p "$HOME"/.cache/conda-apptainer "$HOME"/.conda-apptainer/
+        BIND_CACHE="--bind=$HOME/.cache/conda-apptainer/:$HOME/.cache/conda/ --bind=$HOME/.conda-apptainer/:$HOME/.conda/"
+        SQUASHFS_FILE=conda-env.squashfs
     else
-	echo "No requirements file found (requirements.txt or environment.yml)"
-	return 1
+        echo "No requirements file found (requirements.txt or environment.yml)"
+        return 1
     fi
 
     # Make the environment directories
@@ -127,17 +127,17 @@ function vea() {
 
     # Do the actual building
     apptainer exec \
-	      --bind="$BASE"/venv:"$PATH_IN" \
-	      $BIND_CACHE \
-	      --cwd "$PWD" \
-	      --bind "$PWD:$PWD" \
-	      --workdir="$BASE"/tmp \
-	      --contain --env LANG=C --env LC_ALL=C \
-	      "$IMG" \
-	      bash -c "$install_command"
+              --bind="$BASE"/venv:"$PATH_IN" \
+              $BIND_CACHE \
+              --cwd "$PWD" \
+              --bind "$PWD:$PWD" \
+              --workdir="$BASE"/tmp \
+              --contain --env LANG=C --env LC_ALL=C \
+              "$IMG" \
+              bash -c "$install_command"
     if [ $? -ne 0 ] ; then
-	echo "Apptainer command failed"
-	return 1
+        echo "Apptainer command failed"
+        return 1
     fi
 
     # Note that this is \$BASE - $BASE gets interperted at the point
@@ -147,9 +147,9 @@ function vea() {
 
     # Make it a squashfs, if mksquashfs is installed.
     if test -z "$NO_SQUASH" && type mksquashfs > /dev/null ; then
-	mksquashfs "$BASE"/venv/ "$BASE/$SQUASHFS_FILE" -quiet
-	BIND_ENV="--bind=\"\$BASE\"/${SQUASHFS_FILE}:${PATH_IN}:ro,image-src=/"
-	rm -r venva/venv/
+        mksquashfs "$BASE"/venv/ "$BASE/$SQUASHFS_FILE" -quiet
+        BIND_ENV="--bind=\"\$BASE\"/${SQUASHFS_FILE}:${PATH_IN}:ro,image-src=/"
+        rm -r venva/venv/
     fi
 
     # Install the `$BASE/exec` wrapper that includes all the options
@@ -157,19 +157,19 @@ function vea() {
     # activates the environments within the container, then runs
     # either a shell or the command given on the command line.
     if [ "$install_type" = pip ]; then
-	echo 'BASE="$(dirname $0)"' > "$BASE"/exec
-	echo apptainer exec $BIND_ENV \
-	     --contain --env LANG=C --env LC_ALL=C \
-	     --cwd '"$PWD"' --bind '"$PWD:$PWD"' '--workdir="$BASE"/tmp' \
-	     "$IMG" 'bash -c "source /venv-apptainer/bin/activate ; \${@:-bash}" - "$@"' \
-	     >> $BASE/exec
+        echo 'BASE="$(dirname $0)"' > "$BASE"/exec
+        echo apptainer exec $BIND_ENV \
+             --contain --env LANG=C --env LC_ALL=C \
+             --cwd '"$PWD"' --bind '"$PWD:$PWD"' '--workdir="$BASE"/tmp' \
+             "$IMG" 'bash -c "source /venv-apptainer/bin/activate ; \${@:-bash}" - "$@"' \
+             >> $BASE/exec
     elif [ "$install_type" = conda ] ; then
-	echo 'BASE="$(dirname $0)"' > "$BASE"/exec
-	echo apptainer exec $BIND_ENV \
-	     --contain --env LANG=C --env LC_ALL=C \
-	     --cwd '"$PWD"' --bind '"$PWD:$PWD"' '--workdir="$BASE"/tmp' \
-	     "$IMG" 'bash -c "source activate /venv-apptainer ; \${@:-bash}" - "$@"' \
-	     >> "$BASE"/exec
+        echo 'BASE="$(dirname $0)"' > "$BASE"/exec
+        echo apptainer exec $BIND_ENV \
+             --contain --env LANG=C --env LC_ALL=C \
+             --cwd '"$PWD"' --bind '"$PWD:$PWD"' '--workdir="$BASE"/tmp' \
+             "$IMG" 'bash -c "source activate /venv-apptainer ; \${@:-bash}" - "$@"' \
+             >> "$BASE"/exec
     fi
     chmod a+x "$BASE"/exec
 
@@ -181,9 +181,9 @@ function vea() {
 
     # In $BASE/bin/, install wrappers for all programs within the environment
     for executable in $(./"$BASE"/exec ls "$PATH_IN"/bin/) ; do
-	echo 'BASE="$(dirname $0)/../"' > "$BASE"/bin/"$executable"
-	echo "$BASE"/exec "$executable" '"$@"' >> "$BASE"/bin/"$executable"
-	chmod a+x "$BASE"/bin/"$executable"
+        echo 'BASE="$(dirname $0)/../"' > "$BASE"/bin/"$executable"
+        echo "$BASE"/exec "$executable" '"$@"' >> "$BASE"/bin/"$executable"
+        chmod a+x "$BASE"/bin/"$executable"
     done
 
     # Activate the environment
